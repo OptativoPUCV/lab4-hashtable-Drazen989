c037610dea13c630fd1028041e8b548dbdeffddc
Drazen989 <163464023+Drazen989@users.noreply.github.com>
2024-04-25 00:06:49 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -112,11 +112,27 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
-    return NULL;
+  if(map==NULL) return NULL;
+  long i;
+  for(i=0 ; i<map->capacity ; i++){
+    if(map->buckets[i]!=NULL && map->buckets[i]->key!=NULL){
+      map->current=i;
+      return map->buckets[i];
+    }
+  }
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
-
-    return NULL;
+  if(map==NULL) return NULL;
+  if(map->current<0 || map->current>=map->capacity) return NULL;
+  long i=map->current+1;
+  while(i<map->capacity){
+    if(map->buckets[i]!=NULL && map->buckets[i]->key!=NULL){
+      map->current=i;
+      return map->buckets[i];
+    }
+    i++;
+  }
+  return NULL;
 }
